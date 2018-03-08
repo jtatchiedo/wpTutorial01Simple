e0bf891a6366bb1980f362def2e2073935a98eb7
Jamie Tudor <jamie.tudor@chiedo.com>
2018-03-08 13:46:26 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Wordpress Tutorial: Simple Theme
---
+++ index.php
@@ -0,0 +1,7 @@
+/*
+ * Theme Name: Simple
+ * Author: Jamie Tudor
+ * Author URI: http://whatever.com
+ * Description: Very Simple Theme
+ * Version: 1.0
+ */
